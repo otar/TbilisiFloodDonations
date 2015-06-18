c8988d955639eb50103c88c732c7056ac96db4db
Otar Chekurishvili <otar@chekurishvili.com>
2015-06-18 16:18:55 +0400

    Heatmap

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var app = {
 
     chart: null,
 
-    initialize: function () {
-        if (app.chart === null) {
+    initialize: function ()
+    {
+
+        if (app.chart === null)
+        {
             app.chart = $('#container');
         }
 
@@ -12,23 +15,116 @@ var app = {
 
     },
 
-    loadData: function (callback) {
-        $.getJSON('data.json', callback);
-    },
-
-    render: function(response)
+    loadData: function (callback)
     {
 
-        if (
-               typeof response === 'undefined'
-            || typeof response.data === 'undefined'
-            || !response.data.length
-        )
+        $.getJSON('data.json', function(response)
         {
-            return;
-        }
+
+            if (
+                   typeof response === 'undefined'
+                || typeof response.data === 'undefined'
+                || !response.data.length
+            )
+            {
+                return;
+            }
+
+            var newData = app.formatData(response.data);
+
+            if (typeof callback === 'function')
+            {
+                callback(newData);
+            }
+
+        });
+
+    },
+
+    formatData: function(data)
+    {
+
+        var ranges = [
+            {
+                from: 0,
+                to: 10,
+                fontSize: 10
+            },
+            {
+                from: 10,
+                to: 25,
+                fontSize: 11
+            },
+            {
+                from: 25,
+                to: 50,
+                fontSize: 12
+            },
+            {
+                from: 50,
+                to: 100,
+                fontSize: 13
+            },
+            {
+                from: 100,
+                to: 300,
+                fontSize: 14
+            },
+            {
+                from: 300,
+                to: 1000,
+                fontSize: 15
+            },
+            {
+                from: 1000,
+                to: 99999999,
+                fontSize: 16
+            },
+        ];
+
+        var newData = [];
+
+        $.each(data, function(index, item)
+        {
+
+            $.each(ranges, function(rangeIndex, range)
+            {
+
+                if (
+                       item.value >= range.from
+                    && item.value <= range.to
+                )
+                {
+                    item.colorValue = rangeIndex + 2;
+                    item.dataLabels = {
+                        style: {
+                            fontSize: range.fontSize + 'px'
+                        }
+                    };
+                    return false;
+                }
+
+            });
+
+            newData.push(item);
+
+        });
+
+        return newData;
+
+    },
+
+    render: function(data)
+    {
 
         var options = {
+            colorAxis: {
+                minColor: '#FFCCCC',
+                maxColor: '#FF0000'
+            },
+            legend: {
+                enabled: false
+            },
             tooltip: {
                 useHTML: true,
                 formatter: function()
@@ -47,21 +143,17 @@ var app = {
             },
             series: [{
                 type: 'treemap',
-                alternateStartingDirection: true,
-                layoutAlgorithm: 'squarified',
+                layoutAlgorithm: 'strip',
                 levels: [{
                     level: 1,
-                    dataLabels: {
-                        style: {
-                            fontSize: '16px'
-                        }
-                    }
+                    borderWidth: 3
                 }],
-                data: response.data
+                data: data
             }],
             title: {
                 text: ''
-            }, credits: {
+            },
+            credits: {
                 enabled: false
             }
         };
